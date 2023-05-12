4c6acb332cec970654d0a6225df5cfce16e8c0b6
artyommargaryan <artyom.margaryan.02@gmail.com>
2023-05-12 19:23:05 +0400

    start version

---
+++ src/main/java/com/picsartacademy/leetcodeclone/LeetcodeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.picsartacademy.leetcodeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeCloneApplication.class, args);
+	}
+
+}
